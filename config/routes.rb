f335287973f8e26a76b686f68e8b6e982b255246
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-16 00:20:32 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :create, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post '/login', to: 'auth#create'
+  post "/signup", to: "users#create"
+  post '/autologin', to: 'auth#autologin'
+
+  get '/logged_in', to: 'auth#logged_in?'
+
 end
